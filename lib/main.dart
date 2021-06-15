cf5076431b20e04b58170a1a308c470a1b9a9163
muhammad-faizan-aslam <muhammad.faizan@benchmatrix.ca>
2021-06-15 14:15:38 +0500

    Day5 Completed (MyRoutes)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_8hour/pages/home_page.dart';
 import 'package:flutter_8hour/pages/login_page.dart';
+import 'package:flutter_8hour/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -38,8 +39,8 @@ class MyApp extends StatelessWidget {
       // initialRoute: "/home",
       routes: {
         "/":(context)=> LoginPage(),
-        "/home":(context)=> HomePage(),
-        "/login": (context)=> LoginPage()
+        MyRoutes.homeRoute:(context)=> HomePage(),
+        MyRoutes.loginRoute: (context)=> LoginPage()
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,38 +1,46 @@
+import 'dart:html';
+
 import "package:flutter/material.dart";
+import 'package:flutter_8hour/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Image.asset("assets/images/login_image.jpg",fit: BoxFit.cover,),
-        SizedBox(height:20),
-        Text("Welcome", style:  
-        TextStyle(fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  ),),
-        SizedBox(height:20),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-          child: Column(children: [
-            TextFormField(
-            decoration : InputDecoration(
-              hintText: "Enter username",
-              labelText: "Username"
-            )
-          ),
-          TextFormField(
-            obscureText: true,
-            decoration : InputDecoration(
-              hintText: "Enter Password",
-              labelText: "Password"
-            )
-          ),
+      body: SingleChildScrollView(
+              child: Column(children: [
+          Image.asset("assets/images/login_image.jpg",fit: BoxFit.cover,),
           SizedBox(height:20),
-          ElevatedButton(onPressed: (){}, child: Text("Login"))
-          ],),
-        )
-      ],)
+          Text("Welcome", style:  
+          TextStyle(fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    ),),
+          SizedBox(height:20),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(children: [
+              TextFormField(
+              decoration : InputDecoration(
+                hintText: "Enter username",
+                labelText: "Username"
+              )
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration : InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password"
+              )
+            ),
+            SizedBox(height:20),
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, MyRoutes.homeRoute);
+            }, child: Text("Login"),
+            style: TextButton.styleFrom(minimumSize: Size(100,50)),)
+            ],),
+          )
+        ],),
+      )
     );
   }
 }
